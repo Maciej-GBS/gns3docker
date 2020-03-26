7fdd0175ced9b30ff3205d0ff5890756c9449099
Maciej-GBS <maciejn_99@o2.pl>
2020-03-26 17:10:57 +0100

    Volume mount project folder

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if [ "$1" = "vnc" ]; then
-	docker run -d --rm gns3:latest vnc
+	docker run -d --rm -v gjn:/root/GNS3/projects gns3:latest vnc
 else
-	docker run -it --rm -d -e DISPLAY --user="$(id --user):$(id --group)" -v /tmp/.X11-unix:/tmp/.X11-unix gns3:latest
+	docker run -d --rm -v gjn:/root/GNS3/projects -it -e DISPLAY --user="$(id --user):$(id --group)" -v /tmp/.X11-unix:/tmp/.X11-unix gns3:latest
 fi
 
