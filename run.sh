cb910403c6e8ff2ed3a228302a694eeb085ee6fc
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-04-08 11:12:08 +0200

    Added localhost to hosts

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 if [ "$1" = "vnc" ]; then
 	docker run \
 		-d --rm \
+		--add-host localhost:127.0.0.1 \
 		-v gjn:/root/GNS3/projects \
 		gns3:latest vnc $2
 else
 	docker run \
 		--rm \
+		--add-host localhost:127.0.0.1 \
 		-v gjn:/root/GNS3/projects \
 		-it -e DISPLAY=$DISPLAY \
 		-e user=$(id --user) \
