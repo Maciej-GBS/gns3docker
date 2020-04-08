fab7706a1ea0a9672110e5bcacad33e3336db115
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-04-08 13:11:37 +0200

    Changed x11 volume for projects

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 if [ "$1" = "vnc" ]; then
 	docker run \
 		-d --rm \
-		--add-host localhost:127.0.0.1 \
 		-v gjn:/root/GNS3/projects \
 		gns3:latest vnc $2
 else
 	docker run \
 		--rm \
-		--add-host localhost:127.0.0.1 \
-		-v gjn:/root/GNS3/projects \
+		-v gjnh:/root/GNS3/projects \
 		-it -e DISPLAY=$DISPLAY \
 		-e user=$(id --user) \
 		-v /tmp/.X11-unix:/tmp/.X11-unix \
