8a5d044c8b42ced71a8fa251476fbad08a191b02
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2020-04-23 13:50:01 +0200

    Verbose entry script

--- entry.sh
+++ entry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ "$1" = "vnc" ] || [ "$2" = "vnc" ]; then
+	echo "Setting up vnc server..."
+
 	Xvfb :1 -screen 0 1600x900x16 &
 	x11vnc -display :1.0 -passwd 1234 &
 
@@ -9,21 +11,25 @@ if [ "$1" = "vnc" ] || [ "$2" = "vnc" ]; then
 fi
 
 if [ "$user" != "" ]; then
+	echo "Creating gienio..."
 	useradd --create-home --user-group --uid $user gienio
 	usermod -aG ubridge gienio
-	usermod -aG wireshark gienio
 	usermod -aG libvirt gienio
+	usermod -aG kvm gienio
+	usermod -aG wireshark gienio
 	ln -s /root/GNS3 /home/gienio/GNS3
 fi
 
 if [ "$1" = "term" ] || [ "$2" = "term" ]; then
 	if [ "$user" != "" ]; then
+		echo "Running as gienio..."
 		su gienio -c "lxterminal"
 	else
 		lxterminal
 	fi
 else
 	if [ "$user" != "" ]; then
+		echo "Running as gienio..."
 		su gienio -c "gns3"
 	else
 		gns3
